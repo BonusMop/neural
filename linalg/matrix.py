87d1e963dd4235dd141edecde9582ad0bedb2b4c
BonusMop <butler.brian.j@live.com>
2019-02-22 17:24:16 -0800

    Adds matrix class (#1)
    * can assign values to matrix
    
    * use properties for encapsulation, not getters
    
    * only take dot product when dimensions are correct
    
    * implement dot product

--- linalg/matrix.py
+++ linalg/matrix.py
@@ -1,11 +1,28 @@
 class Matrix:
    
     def __init__(self, rows, columns):
-        self.rows = rows
-        self.columns = columns
+        self._rows = rows
+        self._columns = columns
+        self._values = [[0]*columns for i in range(rows)]
     
-    def get_rows(self):
-        return self.rows
+    @property
+    def rows(self):
+        return self._rows
 
-    def get_columns(self):
-        return self.columns
+    @property
+    def columns(self):
+        return self._columns
+
+    @property
+    def values(self):
+        return self._values
+
+    def dot(self, other):
+        if (self.columns != other.rows):
+            raise ValueError("Row count of input matrix must match column count of this matrix")
+        result = Matrix(self.rows, other.columns)
+        for i in range(self.rows):
+            for j in range(other.columns):
+                for k in range(self.columns):
+                    result.values[i][j] += self._values[i][k]*other.values[k][j]
+        return result
--- test/test_matrix.py
+++ test/test_matrix.py
@@ -12,6 +12,49 @@ class TestMatrix(unittest.TestCase):
         # assert
         self.assertEqual(m.rows,rows)
         self.assertEqual(m.columns,columns)
+    
+    def test_assignValues(self):
+        # arrange
+        num_rows = 3
+        num_columns = 2
+        m = lam.Matrix(num_rows, num_columns)
+        # act
+        for row in range(num_rows):
+            for column in range(num_columns):
+                m.values[row][column] = (row+1)*(column+1)
+        # assert
+        self.assertEqual(m.values[0][0], 1)
+        self.assertEqual(m.values[2][1], 6)
+    
+    def test_dotThrowsWithDimensionMismatch(self):
+        # arrange
+        m1 = lam.Matrix(3, 2)
+        m2 = lam.Matrix(5, 1)
+        # act
+        # assert
+        with self.assertRaises(ValueError) as context:
+            m1.dot(m2)
+        self.assertEqual(str(context.exception),"Row count of input matrix must match column count of this matrix")
+        
+    def test_dotProduct(self):
+        # arrange
+        m1 = lam.Matrix(2,3)
+        m1.values[0] = [-1,8,3]
+        m1.values[1] = [12,4,0]
+        m2 = lam.Matrix(3,1)
+        m2.values[0] = [-1]
+        m2.values[1] = [0]
+        m2.values[2] = [1]
+        # act
+        result = m1.dot(m2)
+        # assert
+        self.assertEqual(result.rows, 2)
+        self.assertEqual(result.columns, 1)
+        self.assertEqual(result.values[0][0],4)
+        self.assertEqual(result.values[1][0],-12)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
