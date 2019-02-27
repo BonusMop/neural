772327f04c8d511f95fb339ee2dbd2391ae40f6c
Brian Butler <butler.brian.j@live.com>
2019-02-27 14:43:24 -0500

    introduce weights and biases

--- network/layer.py
+++ network/layer.py
@@ -1,9 +1,24 @@
+import linalg.matrix as lam
+
 class Layer:
 
-    def __init__(self,size):
-        self._count= size
+    def __init__(self,size,predecessor=None):
+        self._count = size
+        self._biases = None
+        self._weights = None
+        if (predecessor != None):
+            self._biases = lam.Matrix(size, 1)
+            self._weights = lam.Matrix(predecessor.count, size)
         return
 
     @property
     def count(self):
         return self._count
+
+    @property
+    def biases(self):
+        return self._biases
+    
+    @property
+    def weights(self):
+        return self._weights
--- test/test_layer.py
+++ test/test_layer.py
@@ -10,3 +10,18 @@ class TestLayer(unittest.TestCase):
         l = netl.Layer(size)
         # Assert
         self.assertEqual(l.count,size)
+
+    def test_weightsAndBiasesWhenPredecesor(self):
+        # Arrange
+        first_size = 5
+        second_size = 10
+        # Act
+        first_layer = netl.Layer(first_size)
+        second_layer = netl.Layer(second_size, first_layer)
+        # Assert
+        self.assertIsNone(first_layer.biases)
+        self.assertIsNone(first_layer.weights)
+        self.assertEqual(second_layer.biases.columns, 1)
+        self.assertEqual(second_layer.biases.rows, second_size)
+        self.assertEqual(second_layer.weights.columns, second_size)
+        self.assertEqual(second_layer.weights.rows, first_size)
